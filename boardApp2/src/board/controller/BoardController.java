e63d94810d86330e775f497ce9ba1667ba2badf0
아뱅 종로 508_13 <  508_13@DESKTOP-1S88OEE>
2021-03-06 19:09:35 +0900

    추가 커밋

--- boardApp2/src/board/controller/BoardController.java
+++ boardApp2/src/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +17,8 @@ import board.dto.BoardDBBean;
 @Controller
 public class BoardController {
 	
+	@Autowired
 	private BoardDAO boardDAO;
-	
-	public void setBoardDAO(BoardDAO boardDAO) {
-		this.boardDAO = boardDAO;
-	}
 	
 	@RequestMapping("/board_list.do")
 	public String listBoard(HttpServletRequest req) {
